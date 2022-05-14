27080fb098c75c753401ba726e2dcbbba517dc2b
Héctor Molinero Fernández <hector@molinero.dev>
2022-05-14 18:41:06 +0200

    Run e2e tests with Docker CLI v20.10.16

--- e2e/run.sh
+++ e2e/run.sh
@@ -5,12 +5,12 @@ export LC_ALL='C'
 
 SCRIPT_DIR="$(CDPATH='' cd -- "$(dirname -- "${0:?}")" && pwd -P)"
 
-CLI_TREEISH='v20.10.15'
+CLI_TREEISH='v20.10.16'
 CLI_REMOTE='https://github.com/docker/cli.git'
 CLI_PATCH="${SCRIPT_DIR:?}/cli.patch"
 CLI_DIR="$(mktemp -d)"
 
-CONTAINER_LOG_TAG="e2e-$(date -u +'%Y%m%d%H%M%S')"
+TEST_ID="e2e-$(date -u +'%Y%m%d%H%M%S')"
 
 cleanup() { ret="$?"; rm -rf "${CLI_DIR:?}"; trap - EXIT; exit "${ret:?}"; }
 trap cleanup EXIT TERM INT HUP
@@ -19,14 +19,14 @@ main() {
 	git clone "${CLI_REMOTE:?}" "${CLI_DIR:?}"
 	git -C "${CLI_DIR:?}" checkout "${CLI_TREEISH:?}"
 	git -C "${CLI_DIR:?}" apply -v "${CLI_PATCH:?}"
-	printf 'CONTAINER_LOG_TAG=%s\n' "${CONTAINER_LOG_TAG:?}" > "${CLI_DIR:?}"/e2e/.env
+	printf 'TEST_ID=%s\n' "${TEST_ID:?}" > "${CLI_DIR:?}"/e2e/.env
 
-	docker build --tag localhost.test/cetusguard:"${CONTAINER_LOG_TAG:?}" "${SCRIPT_DIR:?}"/../
+	docker build --tag localhost.test/cetusguard:"${TEST_ID:?}" "${SCRIPT_DIR:?}"/../
 	( cd "${CLI_DIR:?}"; make -f "${CLI_DIR:?}"/docker.Makefile test-e2e-non-experimental; ) || ret="$?"
 
-	journalctl --no-pager --output=cat CONTAINER_TAG="${CONTAINER_LOG_TAG:?}"
-	test -n "$(journalctl --output=cat CONTAINER_TAG="${CONTAINER_LOG_TAG:?}" | head -n1)"
-	test -z "$(journalctl --output=cat CONTAINER_TAG="${CONTAINER_LOG_TAG:?}" | grep -v '^\(WARNING\|INFO\|DEBUG\):')"
+	journalctl --no-pager --output=cat CONTAINER_TAG="${TEST_ID:?}"
+	test -n "$(journalctl --output=cat CONTAINER_TAG="${TEST_ID:?}" | head -n1)"
+	test -z "$(journalctl --output=cat CONTAINER_TAG="${TEST_ID:?}" | grep -v '^\(WARNING\|INFO\|DEBUG\):')"
 
 	exit "${ret:-0}"
 }
