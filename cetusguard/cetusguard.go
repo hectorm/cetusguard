823ae946fe9f82ab56747c018c5d380e2571c9fc
Héctor Molinero Fernández <hector@molinero.dev>
2024-04-07 01:40:08 +0200

    Do not print log on ECONNREFUSED error, causes some e2e tests to fail on Docker 26.0.0

--- cetusguard/cetusguard.go
+++ cetusguard/cetusguard.go
@@ -256,7 +256,7 @@ func (cg *Server) handleValidRequest(wri http.ResponseWriter, req *http.Request)
 	}
 
 	res, err := cg.backendHttpClient.Transport.RoundTrip(newReq)
-	if errors.Is(err, context.Canceled) {
+	if errors.Is(err, context.Canceled) || errors.Is(err, syscall.ECONNREFUSED) {
 		mWri.WriteHeader(http.StatusBadGateway)
 		return nil
 	} else if err != nil {
