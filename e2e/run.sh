c3b589a3dce2551e1166d2b008e0e9991c035c35
Héctor Molinero Fernández <hector@molinero.dev>
2025-10-11 17:53:36 +0200

    Run e2e tests with Docker CLI v28.5.1

--- e2e/run.sh
+++ e2e/run.sh
@@ -5,7 +5,7 @@ export LC_ALL='C'
 
 SCRIPT_DIR="$(CDPATH='' cd -- "$(dirname -- "${0:?}")" && pwd -P)"
 
-CLI_TREEISH='v28.0.1'
+CLI_TREEISH='v28.5.1'
 CLI_REMOTE='https://github.com/docker/cli.git'
 CLI_PATCH="${SCRIPT_DIR:?}/cli.patch"
 CLI_DIR="$(mktemp -d)"
@@ -26,7 +26,7 @@ main() {
 
 	printf 'TEST_ID=%s\n' "${TEST_ID:?}" > "${CLI_DIR:?}"/.env
 	docker build --tag localhost.test/cetusguard:"${TEST_ID:?}" "${SCRIPT_DIR:?}"/../
-	( cd "${CLI_DIR:?}"; make -f "${CLI_DIR:?}"/docker.Makefile test-e2e-non-experimental; ) || ret="$?"
+	( cd "${CLI_DIR:?}"; make -f "${CLI_DIR:?}"/docker.Makefile test-e2e-local; ) || ret="$?"
 
 	journalctl --no-pager --output=cat CONTAINER_TAG="${TEST_ID:?}"
 	test -n "$(journalctl --output=cat CONTAINER_TAG="${TEST_ID:?}" | head -n1)"
